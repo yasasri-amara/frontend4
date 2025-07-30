a1456160ff5163fdc9d569da614ebca7773296ce
yasasri-amara <2400031126@kluniversity.in>
2025-07-30 11:59:23 +0530

    version1

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ function App() {
       <h1>Welcome to frontend class in klu</h1>
       <h2> Today's topic is Introduction to React</h2>
       <h3>Today's date is 29.07.25</h3>
-      <h4>Yasasri</h4>
+      <h4>2400031126-Yasasri</h4>
       <Link to="/page1">Go To Page1</Link>
       <br/><br/>
       <Link to="/page2">Go To Page2</Link>
             <br/><br/>
       <Link to="/page3">Go To Page3</Link>
+                  <br/><br/>
+      <Link to="/page4">Go To Page4</Link>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Page1 from './Page1.jsx'
 import Page2 from './Page2.jsx'
 import Page3 from './Page3.jsx'
+import Page4 from '../Page4.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="frontend4">
@@ -16,7 +17,7 @@ createRoot(document.getElementById('root')).render(
   </Route>
   <Route path='/page2'element={<Page2/>}></Route>
    <Route path='/page3'element={<Page3/>}></Route>
-    
+    <Route path='/page4'element={<Page4/>}></Route>
   </Routes>
   </BrowserRouter>
 )
